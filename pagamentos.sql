2f0b37b85303d1c51c75e39717c4317dd010fc39
Nathan Martins Oliveira <150961355+Nathan-M-O@users.noreply.github.com>
2025-03-21 21:47:51 -0300

    Update pagamentos.sql

--- pagamentos.sql
+++ pagamentos.sql
@@ -1,10 +1,8 @@
 CREATE TABLE pagamentos (
-    cod_pagamento INT AUTO_INCREMENT PRIMARY KEY, 
-    cod_ocupacao INT,                             
+    cod_pagamento INT AUTO_INCREMENT PRIMARY KEY,                       
     cod_cliente INT,                              
     valor_pag DECIMAL(10,2) NOT NULL,             
     forma_pag VARCHAR(20) NOT NULL,               
     data_pag DATETIME DEFAULT CURRENT_TIMESTAMP,  
-    FOREIGN KEY (cod_ocupacao) REFERENCES ocupacao(cod_ocup) ON DELETE CASCADE,
     FOREIGN KEY (cod_cliente) REFERENCES clientes(cod_cliente) ON DELETE CASCADE
 );
